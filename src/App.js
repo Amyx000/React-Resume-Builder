0e6908289d6df79c08ff47dececad9060b6665cb
Amyx000 <armankazi111@gmail.com>
2022-10-03 00:48:06 +0530

    file added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css"
+import Home from "./Home";
+
+function App() {
+
+  return (
+    <>
+     <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Home (){
+    return(
+        <>
+            <div>React Resume Builder</div>
+            <button>Get Started</button>
+        </>
+    )
+}
+
+export default Home
